5d1760ca3e3c0a0a5ac088a3da52b4b9ec957bde
kanemototomoki <kanemototomoki@gmail.com>
2021-02-21 22:37:37 +0900

    update

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,17 +1,37 @@
-module.exports = {
-  mode: 'development',
-  entry: './src/main.ts',
+import path from 'path';
+
+const mode = process.env.NODE_ENV || 'development';
+
+const config = {
+  mode,
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    compress: true,
+    port: 7777,
+    hot: true,
+  },
+  watchOptions: {
+    aggregateTimeout: 500,
+    ignored: /node_modules/,
+  },
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         use: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [
-      '.ts', '.js',
-    ],
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
+
+module.exports = config;
