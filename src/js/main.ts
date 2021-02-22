8eb73d5a9948c09ad8b141b28b0ec87e1fb8c337
kanemototomoki <kanemototomoki@gmail.com>
2021-02-22 21:11:04 +0900

    update: main.ts

--- src/js/main.ts
+++ src/js/main.ts
@@ -29,3 +29,57 @@ const showFullName = (person: Person): string => {
 };
 
 console.log(showFullName(takashi));
+
+const years: number[] = [2020, 2021];
+// tuple
+const user: [string, number] = ['tom', 21];
+
+const map = new Map<string, number>([
+  ['takashi', 31],
+  ['daiki', 11],
+]);
+
+for (const [key, value] of map) {
+  console.log(`${key}: ${value}`);
+}
+
+type Person2 = {
+  name: string;
+  favorite: string;
+};
+
+// Partial(ユーティリティ)を使用すると省略できる
+const wzz: Partial<Person2> = { name: "foo" };
+
+// Readonly(ユーティリティ)で要素の書き換えを不可に
+const boo: Readonly<Person2> = { name: "foo", favorite: 'bar' };
+
+// APIのレスポンスなどの場合に使える(keyが可変の文字列だったり)
+const postalCode: { [key: string]: string } = {
+  "602-0000": "京都市上京区",
+  "602-0827": "京都市上京区相生町",
+  "602-0828": "京都市上京区愛染寺町",
+  "602-0054": "京都市上京区飛鳥井町",
+};
+
+// AかつB
+type Twitter = {
+  twitterId: string;
+};
+
+type Instagram = {
+  instagramId: string;
+};
+
+const someUser: Twitter & Instagram = {
+  twitterId: 'foo',
+  instagramId: 'bar',
+};
+
+// 型合成もできる
+type Sns = Twitter & Instagram;
+
+const someUser2: Sns = {
+  twitterId: 'foo',
+  instagramId: 'bar',
+};
