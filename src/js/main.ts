2cf53eab5d02adc5da3b83a0336ca4996385a0a8
kanemototomoki <kanemototomoki@gmail.com>
2021-02-22 18:51:53 +0900

    update: main.ts

--- src/js/main.ts
+++ src/js/main.ts
@@ -4,8 +4,28 @@ const sum = (a: number, b: number): number => {
   return a + b;
 };
 
-const num: number = sum(10, 5);
 
+const num: number = sum(10, 50);
 console.log(num);
 
 
+type Person = {
+  name: string;
+  age: number;
+  favorite?: string;
+}
+
+const takashi: Person = {
+  name: 'takashi',
+  age: 24,
+  favorite: '芋',
+};
+
+const showFullName = (person: Person): string => {
+  const { name, age, favorite } = person;
+  return favorite
+    ? `${name}は${age}歳です。好きなものは${favorite}です。`
+    : `${name}は${age}歳です。`;
+};
+
+console.log(showFullName(takashi));
